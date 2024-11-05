300dea9a62e0f61d035c3b503b9f3d3c81a9fbc6
jeffalewis42 <jeffalewis42@gmail.com>
2024-11-04 21:32:08 -0500

    fixed where the SuperCoin function was named superCoin

--- src/Components/SuperCoin.jsx
+++ src/Components/SuperCoin.jsx
@@ -1,12 +1,11 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
-const superCoin = () => {
+const SuperCoin = () => {
 const [superCoins, setSuperCoins] = useState (0);
 
 const cartItems = useSelector(state => state.cart.cartItems);
-
-const totalAmount = cartItems.reduce((total,item) => total + item.price * item.quantity, 0);
+const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
 
 useEffect (() => {
     if (totalAmount >= 100 && totalAmount < 200) {
@@ -21,10 +20,12 @@ useEffect (() => {
 }, [totalAmount]);
 
 return (
+    <>
 <div className="super-coins" style={{textAlign:'center'}}>
   <h2 className="super-coins-title">Super Coins</h2>
   <p className="super-coins-info">You will earn {superCoins} super coins with this purchase.</p>
 </div>
+</>
 );
 };
 
